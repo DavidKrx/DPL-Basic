f08e71924dfc450b67dd299190fd5facf53dc24a
DavidKrx <davidcm097@gmail.com>
2025-02-09 15:57:12 +0000

    Jar y war generados

--- src/main/java/com/cifp/myikea/repository/ProvinciaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cifp.myikea.repository;
-
-import com.cifp.myikea.model.Provincia;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProvinciaRepository extends JpaRepository<Provincia, Integer> {
-}
